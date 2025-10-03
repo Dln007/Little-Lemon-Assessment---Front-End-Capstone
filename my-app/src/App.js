a529631ad93e544c248723e5b8fb12318b5ccb02
Dln007 <dylan.ammundsen@gmail.com>
2025-10-03 19:44:38 +1300

    Components set up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 import Nav from './Nav';
-import Hero from './Hero';
-import Highlights from './Highlights';
-import Testimonials from './Testimonials';
-import About from './About';
 import Footer from './Footer';
+
+import HomePage from './HomePage';
+import BookingPage from './BookingPage';
 
 function App(){
   return(
+    <BrowserRouter>
     <div className='App'>
-    <header>
-      <Nav />
-    </header>
+      <header>
+        <Nav />
+      </header>
 
-    <main>
-      <Hero />
-      <Highlights />
-      <Testimonials />
-      <About />
-    </main>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Booking" element={<BookingPage />} />
+        </Routes>
+      </main>
 
-    <Footer />
+      <Footer />
+
     </div>
+    </BrowserRouter>
   )
 }
 
--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Nav(){
     return(
         <nav aria-label="Primary">
         <ul className="nav-links">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/menu">Menu</a></li>
-            <li><a href="/reservations">Reservations</a></li>
-            <li><a href="/orderOnline">Order Online</a></li>
-            <li><a href="/logIn">Login</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/reservations">Reservations</Link></li>
+            <li><Link to="/order">Order Online</Link></li>
+            <li><Link to="/login">Login</Link></li>
         </ul>
         </nav>
     );
