f24011b167902d258ae32dbfaf1d8df9d46b0dfa
Dln007 <dylan.ammundsen@gmail.com>
2025-10-07 17:54:30 +1300

    Completed exercise: Adding Form Validation:

--- my-app/src/BookingForm.js
+++ my-app/src/BookingForm.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function BookingForm({ availableTimes, dispatch, submitForm }) {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [guests, setGuests] = useState(1);
-  const [occasion, setOccasion] = useState("Birthday");
+  const [occasion, setOccasion] = useState("");
+  const [isFormValid, setIsFormValid] = useState (false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = { date, time, guests, occasion };
     submitForm(formData); 
   };
+
+  useEffect (() => {
+    const valid = date && time && guests >=1 && guests <=10 && occasion !=="";
+    setIsFormValid (valid);
+  }, [date, time, guests, occasion]);
 
   return (
     <form onSubmit={handleSubmit} style={{ display: "grid", maxWidth: "320px", gap: "12px" }}>
@@ -25,6 +31,7 @@ function BookingForm({ availableTimes, dispatch, submitForm }) {
           dispatch({ type: "UPDATE_TIMES", date: selectedDate });
         }}
         required
+        min={new Date().toISOString().split("T")[0]} // ensures no past dates are selected
       />
 
       <label htmlFor="res-time">Choose a Time</label>
@@ -42,19 +49,20 @@ function BookingForm({ availableTimes, dispatch, submitForm }) {
         min={1}
         max={10}
         value={guests}
-        onChange={(e) => setGuests(e.target.value)}
+        onChange={(e) => setGuests(Number(e.target.value))}
         required
       />
 
       <label htmlFor="occasion">Occasion</label>
       <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)} required>
+        <option value="">Select An Occasion</option>
         <option>Birthday</option>
         <option>Anniversary</option>
         <option>Engagement</option>
         <option>Wedding</option>
       </select>
 
-      <button type="submit">Make Your Reservation!</button>
+      <button type="submit" disabled={!isFormValid}>Make Your Reservation!</button>
     </form>
   );
 }
