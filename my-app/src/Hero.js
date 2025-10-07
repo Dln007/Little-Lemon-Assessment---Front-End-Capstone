518d8bd0ba8f2c8cbf1867a2334822d634618b7b
Dln007 <dylan.ammundsen@gmail.com>
2025-10-07 19:41:03 +1300

    Made the Reserve a table button on the homepage actually function haha

--- my-app/src/Hero.js
+++ my-app/src/Hero.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Hero(){
     return(
       <section className="hero">
         <h1>Welcome to Little Lemon!</h1>
         <p>Reserve a table and enjoy our delicious meals!</p>
+        <Link to="/booking">
         <button>Reserve a table</button>
+        </Link>
         {/* add image here*/}
       </section>
     );
